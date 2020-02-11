1fa73f760e874d674dc9a6f44dc8a4468f68c8c1
Jerry Morrison <1fish2@users.noreply.github.com>
2020-02-10 22:55:08 -0800

    init pyenv before using it

--- script/startup.sh
+++ script/startup.sh
@@ -2,9 +2,11 @@
 
 cd "$HOME/borealis" || { echo "Failure"; exit 1; }
 git pull origin
-pip install --upgrade borealis-fireworker
 
 # init pyenv
 [[ -f "$HOME/.bash_aliases" ]] && source "$HOME/.bash_aliases"
+
+pip install --upgrade borealis-fireworker
+pyenv rehash
 
 python -m borealis.fireworker
